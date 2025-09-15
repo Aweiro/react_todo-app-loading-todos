062c01e2b1fd49761556c339110ed2de979809c0
Aweiro <arsenskott@gmail.com>
2025-09-15 16:42:49 +0300

    add task solution

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,14 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 3493;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
+
+export function deleteTodos(postId: number) {
+  return client.delete(`/todos/${postId}`);
+}
 
 // Add more methods here
